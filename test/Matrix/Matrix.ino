10290e8d00a1dd754d2b521199f608df84f21b86
John Johnston <john@longjohn.co.nz>
2012-04-07 12:33:57 +1200

    Rotary Encoder

--- test/Matrix/Matrix.ino
+++ test/Matrix/Matrix.ino
@@ -1,29 +1,30 @@
 #include <RGB.h>
 #include <RGBMatrix.h>
 
-int NUM_BOARDS = 2;
+int NUM_BOARDS = 8;
+int RAIN_SIZE = 40;
 
-char colors[8] = { 
-  BLACK, RED, GREEN, BLUE, ORANGE, MAGENTA, TEAL, WHITE };
+char colors[8] = { BLACK, RED, GREEN, BLUE, ORANGE, MAGENTA, TEAL, WHITE};
+const byte smileyFace[] = {0x0,0x26,0x46,0x40,0x40,0x46,0x26,0x0};
 
-const byte smileyFace[] = {
-  0x0,0x26,0x46,0x40,0x40,0x46,0x26,0x0};
-
-unsigned int rainPositions[16];
+unsigned int rainPositions[40];
 
 void setup()
 {
   delay(5000); 
   Serial.begin(115200);
 
+  pinMode(7, INPUT);
+
   // Clear array
-  for(int i=0;i<16;i++)
+  for(int i=0;i<RAIN_SIZE;i++)
   {
     rainPositions[i] = 0;
-
   }
 
   RGBMatrix.begin(NUM_BOARDS);
+
+  RGBMatrix.clear();
   displayBoards();
 
   randomSeed(analogRead(0));
@@ -31,7 +32,7 @@ void setup()
 
 void displayMessage(void)
 {
-  RGBMatrix.scroll("HELLO ABIGAIL", GREEN, 40);
+  RGBMatrix.scroll("JOHN WAS HERE!", GREEN, 40);
 }
 
 void displayBoards(void)
@@ -59,7 +60,7 @@ void displayRandom()
     RGBMatrix.fillPixel(random(NUM_BOARDS), random(8), random(8), colors[random(8)]); 
     RGBMatrix.display();
   }
-  delay(100);
+  delay(1);
 
 }
 
@@ -117,7 +118,7 @@ void displayFillRow()
       }
 
       RGBMatrix.display();
-      delay(50);        
+      delay(1);        
     }
   }
 }
@@ -136,7 +137,7 @@ void displayFillColumn()
       {
         RGBMatrix.fillColumn(board, column, colors[color]);
         RGBMatrix.display();
-        delay(50);            
+        delay(5);            
       }
     }
 
@@ -146,7 +147,7 @@ void displayFillColumn()
       {
         RGBMatrix.fillColumn(board, column, BLACK);
         RGBMatrix.display();
-        delay(50);            
+        delay(5);            
       }
     }    
   }
@@ -242,46 +243,49 @@ void displayFace(void)
   int count=0;
 
 
-    for(int color=1; color<255; color++)
+  for(int color=1; color<255; color++)
+  {
+
+    for(int board=0; board<NUM_BOARDS; board++)
     {
 
-      for(int board=0; board<NUM_BOARDS; board++)
+      for(int column=0; column<NUM_COLUMNS; column++)
       {
-
-        for(int column=0; column<NUM_COLUMNS; column++)
+        for(int row=0; row<NUM_ROWS; row++)
         {
-          for(int row=0; row<NUM_ROWS; row++)
+          if( smileyFace[(7-column)] & (1<<row))
           {
-            if( smileyFace[(7-column)] & (1<<row))
-            {
-              RGBMatrix.fillPixel(board, row, column, color);
-
-              //rainPositionsBuffer[rainPositions].pixel[row*8+column]=color;
-            }
-            else 
-            {
-              RGBMatrix.fillPixel(0, row, column, BLACK);
-              //rainPositionsBuffer[rainPositions].pixel[row*8+column]=BLACK;
-            }
+            RGBMatrix.fillPixel(board, row, column, color);
+          }
+          else 
+          {
+            RGBMatrix.fillPixel(0, row, column, BLACK);
           }
         }
       }
+    }
 
-      RGBMatrix.display();
-      delay(50);
+    RGBMatrix.display();
+    delay(50);
 
   }
 
 }
 
-
+/*
 void displayRain()
 {
 
   unsigned int randomNumber, currentBoard, currentColumn, rowPosition;
   unsigned int count;
-
-  randomNumber = 0; currentBoard = 0; currentColumn = 0; rowPosition = 0; count=0;
+  unsigned int totalColumns;
+ 
+  randomNumber = 0; 
+  currentBoard = 0; 
+  currentColumn = 0; 
+  rowPosition = 0; 
+  count=0;
+  totalColumns = 8 * NUM_BOARDS;
 
   RGBMatrix.clear();
 
@@ -290,15 +294,15 @@ void displayRain()
   {
 
     // Generate random number indicates column rain drop will start from
-      randomNumber = random(16);
+    randomNumber = random(totalColumns);
 
-      if( rainPositions[randomNumber] == 0)
-      {
-        rainPositions[randomNumber] = 1;
-      }
-    
+    if( rainPositions[randomNumber] == 0)
+    {
+      rainPositions[randomNumber] = 1;
+    }
+
     // Increase rain drop row positions
-    for( int i=0; i<16; i++)
+    for( int i=0; i<totalColumns; i++)
     {
       if(rainPositions[i] > 0)
       {
@@ -306,7 +310,7 @@ void displayRain()
       }
 
       // Reset column once drop has left the rainPositions
-      if( rainPositions[i] > 10)
+      if( rainPositions[i] > totalColumns+2)
       {
         rainPositions[i] = 0;
       }
@@ -314,22 +318,16 @@ void displayRain()
 
     // Display rain drops
     // NOTE: Need fix for NUM_BOARDS
- 
-    for( int i=0; i<16; i++ )
+
+    for( int i=0; i<totalColumns; i++ )
     {
       // Set current board and column
-      if( i > 7 )
+      if( i % 8 = 0 )
       {
-        currentBoard = 1;
-        currentColumn = i-7;
+        currentBoard++;
+        currentColumn = i - (currentBoard * 8);
       }
-      else
-      {
-        currentBoard = 0;
-        currentColumn = i;
-      }
-
-
+      
       // Rain drop waiting to be displayed
       if( rainPositions[i] > 0 )
       {
@@ -339,14 +337,14 @@ void displayRain()
         {
 
           // Rain drop exists on current row
-          if( row == rainPositions[i]-1)
+          if( rainPositions[i]-1 = row)
           {
             // Front of drop color
             if( rainPositions[i]-1 < 8 )
             {
               RGBMatrix.fillPixel(currentBoard, rainPositions[i]-1, currentColumn, TEAL);  
             }
-            
+
             // Behind drop color
             if( rainPositions[i]-1 > 0 )
             {
@@ -383,25 +381,157 @@ void displayRain()
 
 
 }
+*/
 
+void displayCrossLines(void)
+{
+  int row = 0;
+
+
+  for( int color=1; color<8; color++)
+  {
+    RGBMatrix.clear();
+
+    for( int column = 0; column<NUM_COLUMNS; column++)
+    {
+
+
+      if( column > 0)
+      {
+        for(int board=0; board<NUM_BOARDS; board++)
+        {
+          RGBMatrix.fillColumn(board, column-1, BLACK);
+        }
+        RGBMatrix.display();
+      }
+
+
+      if( row > 0)
+      {
+        for(int board=0; board<NUM_BOARDS; board++)
+        {
+          RGBMatrix.fillRow(board, row-1, BLACK);
+        }
+        RGBMatrix.display();
+      }
+
+      for(int board=0; board<NUM_BOARDS; board++)
+      {
+
+        RGBMatrix.fillColumn(board, column, colors[color]);
+        RGBMatrix.fillRow(board, row, colors[color]);
+
+      }   
+      RGBMatrix.display();
+
+      row++;
+      if(row > NUM_ROWS)
+      {
+        row = 0;
+      }
+
+      delay(5);
+    } 
+
+    row = NUM_ROWS-2;
+
+
+    for( int column = NUM_COLUMNS-2; column>0; column--)
+    {
+
+
+      if( column < NUM_COLUMNS)
+      {
+        for(int board=0; board<NUM_BOARDS; board++)
+        {
+          RGBMatrix.fillColumn(board, column+1, BLACK);
+        }
+        RGBMatrix.display();
+      }
+
+
+      if( row <= NUM_ROWS)
+      {
+        for(int board=0; board<NUM_BOARDS; board++)
+        {
+
+          RGBMatrix.fillRow(board, row+1, BLACK);
+        }
+        RGBMatrix.display();
+      }
+
+      for(int board=0; board<NUM_BOARDS; board++)
+      {
+
+        RGBMatrix.fillColumn(board, column, colors[color]);
+        RGBMatrix.fillRow(board, row, colors[color]);
+      }   
+      RGBMatrix.display();
+
+      row--;
+      if(row < 0)
+      {
+        row = NUM_ROWS-1;
+      }
+
+      delay(5);
+    } 
+
+
+  }
+
+
+
+}
+
+
+void displayLevel( void )
+{
+  
+  RGBMatrix.clear();
+  
+  for(char i=0; i<10; i++ )
+  {
+    RGBMatrix.fillNumber(0, i, GREEN);
+    RGBMatrix.display();
+    
+    delay(1000);
+  }
+  
+}
+    
+    
 
 void loop()
 {
-
-  //displayMessage(); 
+ //Serial.println(analogRead(5)/10);
+  
+  displayBoards();
+  
+  //displayLevel();
+  
   //  
-  displayRandom();
+  //displayRandom();
   //displayFrame();
-  //displayFillColors();
-  //displayFillRow();
-  //displayFillColumn();
+  displayFillColors();
+  displayFillRow();
+  displayFillColumn();
   //displayFrames();
 
-  //displayFace();
-
+  displayFace();
   //displayRain();
-  displayBoards();
+
+  displayCrossLines();
+  displayMessage(); 
+  
+  
+
+
 }
+
+
+
+
 
 
 
